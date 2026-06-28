198bc68210a02136bcf030d09598b8f3dbc4036f
Binho <binho3399@users.noreply.github.com>
2026-06-28 11:12:57 +0700

    Add responsive bg test glass grid

---
+++ app/bg-test/page.tsx
@@ -0,0 +1,9 @@
+import BgTestStage from '../../components/BgTestStage';
+
+export const metadata = {
+  title: 'Background Test',
+};
+
+export default function Page() {
+  return <BgTestStage />;
+}
